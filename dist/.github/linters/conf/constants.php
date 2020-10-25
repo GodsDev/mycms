d772a8169474f713990288e207860459002b9a0f
Stanislav Rejthar <rejthar@stanislavrejthar.com>
2020-10-25 18:05:18 +0100

    * MyTableLister: TODOs for CRS2 * .gitignore: .php_cs.cache * phpstan include structure (mycms includes dist, github/phpstan includes local)

--- dist/.github/linters/conf/constants.php
+++ dist/.github/linters/conf/constants.php
@@ -8,13 +8,13 @@
  * 
  */
 
-define('DEBUG_VERBOSE', false);
-define('DEFAULT_LANGUAGE', 'cs');
-define('EMAIL_ADMIN', 'test@test.com');
-define('NOTIFY_FROM_ADDRESS', 'abc');
-define('NOTIFY_FROM_NAME', 'abc');
-define('PATH_MODULE', 10);
-define('SMTP_HOST', 'localhost');
-define('SMTP_PORT', 25);
-define('TAB_PREFIX', 'abc');
-define('UNDER_CONSTRUCTION', false);
+//define('DEBUG_VERBOSE', false);
+//define('DEFAULT_LANGUAGE', 'cs');
+//define('EMAIL_ADMIN', 'test@test.com');
+//define('NOTIFY_FROM_ADDRESS', 'abc');
+//define('NOTIFY_FROM_NAME', 'abc');
+//define('PATH_MODULE', 10);
+//define('SMTP_HOST', 'localhost');
+//define('SMTP_PORT', 25);
+//define('TAB_PREFIX', 'abc');
+//define('UNDER_CONSTRUCTION', false);
